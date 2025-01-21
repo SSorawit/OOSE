d87d8b6c2fce506082762a6c18b4ae8be5153516
SSorawit <sowarit007@gmail.com>
2025-01-21 21:39:08 +0700

    DONE

--- MID/ATMAction.java
+++ MID/ATMAction.java
@@ -6,7 +6,7 @@ public interface ATMAction {
 
     boolean depositable(BankAccount loggedInAccount, double amount, boolean isBTC);
 
-    boolean transferable(BankAccount loggedInAccount, BankAccount recipient, double amount);
+    boolean transferable(BankAccount loggedInAccount, BankAccount recipient, double amount, boolean isBTC);
 
     double exchangrateBTC(double exchangrate);
 
